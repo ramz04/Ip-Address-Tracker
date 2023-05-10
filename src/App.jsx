3dece8b6dd0e1e41e761a78957ac90347c977a41
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-05-10 20:34:59 +0000

    Added Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
       <section className='App'>
         <div className='header' >
           <picture>
-            <source media='min-width: 1024px' srcSet={background} />
+            <source media='(min-width: 1024px)' srcSet={background} />
             <img src={ backgroundMobile } className='image' alt="Background" />
           </picture>
         </div>
-        <article className='header--content'>
+        <article className='header--search'>
            <h1>IP Address Tracker</h1>
            <form>
             <input id="ipaddress" name="ipaddress" type="text" placeholder="Search for any IP address or domain" />
@@ -22,6 +22,27 @@ function App() {
             </button>
            </form>
         </article>
+        <div className='header--content'>
+          <article className='header--content-wrapper shadow max-w-5xl'>
+            <div className='header--content-items'>
+              <h2>IP Address</h2>
+              <p>192.212.174.101</p>
+            </div>
+            <div className='header--content-items'>
+              <h2>Location</h2>
+              <p>Brooklyn, NY 10001</p>
+            </div>
+            <div className='header--content-items'>
+              <h2>Timezone</h2>
+              <p>UTC-05:00</p>
+            </div>
+            <div className='header--content-last'>
+              <h2>ISP</h2>
+              <p>SpaceX Starlink</p>
+            </div>
+
+          </article>
+        </div>
       </section>
     </>
   )
