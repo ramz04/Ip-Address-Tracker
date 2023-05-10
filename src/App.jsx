0880852e82381f67da1bbe47e76fb54bd7f3780a
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-05-10 18:40:19 +0000

    Added The Search Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import arrow from './assets/images/icon-arrow.svg'
+import background from './assets/images/pattern-bg-desktop.png'
+import backgroundMobile from './assets/images/pattern-bg-mobile.png'
 
 function App() {
 
   return (
-    <div className="text-7xl">
-      Hello Vite + React!
-    </div>
+    <>
+      <section className='App'>
+        <div className='header' >
+          <picture>
+            <source media='min-width: 1024px' srcSet={background} />
+            <img src={ backgroundMobile } className='image' alt="Background" />
+          </picture>
+        </div>
+        <article className='header--content'>
+           <h1>IP Address Tracker</h1>
+           <form>
+            <input id="ipaddress" name="ipaddress" type="text" placeholder="Search for any IP address or domain" />
+            <button type="submit">
+              <img src={arrow} alt="" />
+            </button>
+           </form>
+        </article>
+      </section>
+    </>
   )
 }
 
